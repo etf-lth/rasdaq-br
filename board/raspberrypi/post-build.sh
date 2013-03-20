eaa06f418a9bc282a8b036d89bf4320f585a732b
Fredrik Ahlberg <fredrik@z80.se>
2013-03-20 22:25:48 +0100

    less...

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -4,31 +4,31 @@ TARGETDIR=$1
 sed -i 's%^root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 
 # Point /bin/sh to /bin/bash
-ln -T -s /bin/bash $TARGETDIR/bin/sh 
+ln -T -s /bin/ash $TARGETDIR/bin/sh 
 
 # Package the /boot partition
 tar -czf $TARGETDIR/../images/boot.tar.gz --exclude=Image -C $TARGETDIR/boot/ .
 
 # remove inittab
-rm $TARGETDIR/etc/inittab
+#rm $TARGETDIR/etc/inittab
 
 #remove rc.conf
-rm $TARGETDIR/etc/init/rc.conf
+#rm $TARGETDIR/etc/init/rc.conf
 
 #add task to mount everything
-cp board/raspberrypi/mount.conf $TARGETDIR/etc/init/
+#cp board/raspberrypi/mount.conf $TARGETDIR/etc/init/
 
 #add task to set hostname
-cp board/raspberrypi/hostname.conf $TARGETDIR/etc/init/
+#cp board/raspberrypi/hostname.conf $TARGETDIR/etc/init/
 
 #add task to start getty on tty1
-cp board/raspberrypi/tty1.conf $TARGETDIR/etc/init/
+#cp board/raspberrypi/tty1.conf $TARGETDIR/etc/init/
 
 # add eth0 dhcp entry into /etc/network/interfaces
-cp board/raspberrypi/interfaces $TARGETDIR/etc/network/
+#cp board/raspberrypi/interfaces $TARGETDIR/etc/network/
 
 # make sure that ntpdate is run before sshd is started
-cp board/raspberrypi/ntpdate.conf $TARGETDIR/etc/init/
+#cp board/raspberrypi/ntpdate.conf $TARGETDIR/etc/init/
 
 # start bluetooth daemon
-cp board/raspberrypi/bluetooth.conf $TARGETDIR/etc/init/
+#cp board/raspberrypi/bluetooth.conf $TARGETDIR/etc/init/
