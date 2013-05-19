978fbd139342f30c2e67dd0756fbf1b14bc978c1
Fredrik Ahlberg <fredrik@z80.se>
2013-05-19 20:12:19 +0200

    wip

--- package/raspberrypi/bootloader/bootloader.mk
+++ package/raspberrypi/bootloader/bootloader.mk
@@ -9,17 +9,9 @@ define BOOTLOADER_INSTALL_TARGET_CMDS
 	cp $(@D)/bootcode.bin $(TARGET_DIR)/boot/bootcode.bin
 	cp $(@D)/fixup.dat $(TARGET_DIR)/boot/fixup.dat
 	# Generate boot config files
-	echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty2 root=/dev/mmcblk0p2 rootfstype=ext4 coherent_pool=6M smsc95xx.turbo_mode=N rootwait quiet" > $(TARGET_DIR)/boot/cmdline.txt
-	echo "disable_overscan=1" > $(TARGET_DIR)/boot/config.txt
-	echo "framebuffer_depth=24" >> $(TARGET_DIR)/boot/config.txt
-	echo "arm_freq=1000" >> $(TARGET_DIR)/boot/config.txt
-	echo "core_freq=500" >> $(TARGET_DIR)/boot/config.txt
-	echo "over_voltage=6" >> $(TARGET_DIR)/boot/config.txt
-	echo "gpu_mem_256=112" >> $(TARGET_DIR)/boot/config.txt
-	echo "gpu_mem_512=368" >> $(TARGET_DIR)/boot/config.txt
-	echo "cma_lwm=16" >> $(TARGET_DIR)/boot/config.txt
-	echo "cma_hwm=32" >> $(TARGET_DIR)/boot/config.txt
-	echo "cma_offline_start=16" >> $(TARGET_DIR)/boot/config.txt
+	echo "dwc_otg.lpm_enable=0 console=ttyS0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait" > $(TARGET_DIR)/boot/cmdline.txt
+	echo "avoid_safe_mode=1" > $(TARGET_DIR)/boot/config.txt
+	echo "boot_delay=0" >> $(TARGET_DIR)/boot/config.txt
 endef
 
 $(eval $(generic-package))
